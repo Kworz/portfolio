eb0560c73465699f15f19b297b6ed69721a01159
Kworz <romain.sapet@metalizz.com>
2023-09-29 16:21:07 +0200

    adjusted shader line color from position

--- src/lib/home/fragment.glsl
+++ src/lib/home/fragment.glsl
@@ -24,7 +24,8 @@ void main()
         float circle = 1.0 - smoothstep(0.9 * circleGrowTimer, 1. * circleGrowTimer, length(pulsePosition.xy - vPosition.xy) * 0.05);
 
         // bright colors
-        vec3 color = vec3(colorFadeTimer * 3.0, vPosition.y * 0.3, 7.0);
+        vec3 color = vec3(pulsePosition.x * 3.0, pulsePosition.y * 0.3, -pulsePosition.z * 8.0 * colorFadeTimer);
+        color = color * colorFadeTimer;
         vec3 coloredLines = (color * colorFadeTimer * lineFill);
         vec3 final = coloredLines = mix(coloredLines, vec3(lineFill * 0.3), 1.- circle * colorFadeTimer);
 
