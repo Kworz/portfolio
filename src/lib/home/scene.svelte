0a5ff353c3deaf07f65e2dbe84f1899a3d0d140e
Kworz <romain.sapet@metalizz.com>
2023-09-29 16:21:23 +0200

    updated responsiveness

--- src/lib/home/scene.svelte
+++ src/lib/home/scene.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
     import { T, useFrame } from '@threlte/core';
-    import { interactivity, useGltf } from '@threlte/extras';
+    import { Align, Center, HTML, interactivity, useGltf } from '@threlte/extras';
     import { spring, tweened } from 'svelte/motion';
     import { Vector3, type Mesh } from 'three';
+    import { quadOut } from 'svelte/easing';
 
     import fragment from "./fragment.glsl?raw";
     import vertex from "./vertex.glsl?raw";
-    import { quadOut } from 'svelte/easing';
 
     interactivity();
 
@@ -24,6 +24,16 @@
         easing: quadOut
     });
 
+    const computeFov = () => {
+        if(window.innerWidth < 768)
+            // as the innerWidth decreases, increase the fov
+            return 40 + (768 - window.innerWidth) / 20;
+        else
+            return 40;
+    }
+
+    let fov = computeFov();
+
     useFrame(() => {
         rotationX.set(Math.sin((performance.now() / 1000)) * 0.01);
         rotationX.set(Math.cos((performance.now() / 1000)) * 0.015);
@@ -34,12 +44,14 @@
 
 </script>
 
-<T.PerspectiveCamera makeDefault fov={40} aspect={2} near={0.1} position={[0, 8, 20]} far={300} on:create={({ ref }) => { ref.lookAt(0, 6, 3.5);}}>
+<svelte:window on:resize={() => fov = computeFov()} />
+
+<T.PerspectiveCamera makeDefault {fov} aspect={2} near={0.1} position={[0, 8, 20]} far={300} on:create={({ ref }) => { ref.lookAt(0, 6, 3.5);}}>
 </T.PerspectiveCamera>
 
-<T.AmbientLight color="white" />
-<T.DirectionalLight color="beige" position={[0, 10, 20]} />
-<T.DirectionalLight color="white" position={[0, 20, 0]} />
+<T.AmbientLight />
+<T.DirectionalLight position={[0, 10, 20]} />
+<T.DirectionalLight position={[0, 20, 0]} />
 
 {#if $model}
     <T.Mesh 
@@ -55,7 +67,7 @@
             pulsePosition.set(point.x, point.y, point.z);
             pulseTimer.set(0, { duration: 0 });
             pulseTimer.set(1, { duration: 3000 });
-          }}
+        }}
     >
         <T.ShaderMaterial
             fragmentShader={fragment}
@@ -67,4 +79,6 @@
             uniforms.pulseTimer.value={$pulseTimer}
         />
     </T.Mesh>
+{:else}
+    <HTML>Chargement...</HTML>
 {/if}
