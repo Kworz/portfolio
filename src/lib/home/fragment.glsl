a1ccfea1f1260aef94bdff26b47913faa5aee194
Kworz <romain.sapet@metalizz.com>
2023-09-29 10:13:49 +0200

    updated shaders to be white on default

--- src/lib/home/fragment.glsl
+++ src/lib/home/fragment.glsl
@@ -1,23 +1,33 @@
 // Credit: https://madebyevan.com/shaders/grid/
 varying vec2 vUv;
 varying vec3 vPosition;
+
+// External vars
 uniform vec3 pulsePosition;
 uniform float pulseTimer;
 
 void main()
 {
-    float coord = vPosition.z * 2.;
+    float coord = vPosition.z * 5.0;
     float line = abs(fract(coord - 0.7) - 0.25) / fwidth(coord);
-    float lineFill = 1.0 - min(line, 1.0);
 
-    float circleGrowTimer = min(pulseTimer * 2., 1.);
-    float colorFadeTimer = 1. - pulseTimer;
-    float circle = 1.0 - smoothstep(0.9 * circleGrowTimer, 1. * circleGrowTimer, length(pulsePosition.xy - vPosition.xy) * 0.05);
+    if (line > 1.0)
+    {
+        gl_FragColor = vec4(1.0, 0.96, 0.95, 0.9);
+    }
+    else
+    {
+        float lineFill = 1.0 - min(line, 1.0);
 
-    // bright colors
-    vec3 color = vec3(vPosition.y * 1.5, 1., 1.);
-    vec3 coloredLines = (color * colorFadeTimer * lineFill);
-    vec3 final = coloredLines = mix(coloredLines, vec3(lineFill * 0.1), 1.- circle * colorFadeTimer);
+        float circleGrowTimer = min(pulseTimer * 2., 1.);
+        float colorFadeTimer = 1. - pulseTimer;
+        float circle = 1.0 - smoothstep(0.9 * circleGrowTimer, 1. * circleGrowTimer, length(pulsePosition.xy - vPosition.xy) * 0.05);
 
-    gl_FragColor = vec4(final, 1.);
+        // bright colors
+        vec3 color = vec3(colorFadeTimer * 3.0, vPosition.y * 0.3, 7.0);
+        vec3 coloredLines = (color * colorFadeTimer * lineFill);
+        vec3 final = coloredLines = mix(coloredLines, vec3(lineFill * 0.3), 1.- circle * colorFadeTimer);
+
+        gl_FragColor = vec4(final, 1.);
+    } 
 }
--- src/lib/home/scene.svelte
+++ src/lib/home/scene.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
     import { T, useFrame } from '@threlte/core';
-    import { OrbitControls, interactivity, useGltf } from '@threlte/extras';
+    import { interactivity, useGltf } from '@threlte/extras';
     import { spring, tweened } from 'svelte/motion';
-    import { Vector3, type CircleGeometry, type DirectionalLight, type Mesh, MeshStandardMaterial } from 'three';
+    import { Vector3, type Mesh } from 'three';
 
     import fragment from "./fragment.glsl?raw";
     import vertex from "./vertex.glsl?raw";
@@ -19,10 +19,10 @@
     const scale = spring(0);
     const opacity = spring(0);
 
-    const pulsePosition = new Vector3()
+    const pulsePosition = new Vector3();
     const pulseTimer = tweened(0, {
         easing: quadOut
-    })
+    });
 
     useFrame(() => {
         rotationX.set(Math.sin((performance.now() / 1000)) * 0.01);
@@ -30,56 +30,41 @@
         scale.set(Math.sin((performance.now() / 1000)) * 0.001 + 0.2);
     });
 
-    $: if($model !== undefined) { scale.set(0.2); opacity.set(1); };
+    $: if($model !== undefined) { scale.set(0.4); opacity.set(1); };
 
 </script>
 
-<T.PerspectiveCamera makeDefault fov={50} aspect={2} near={0.1} position={[0, 4, 20]} far={300} on:create={({ ref }) => { ref.lookAt(0, 3, 3.5);}}>
+<T.PerspectiveCamera makeDefault fov={40} aspect={2} near={0.1} position={[0, 8, 20]} far={300} on:create={({ ref }) => { ref.lookAt(0, 6, 3.5);}}>
 </T.PerspectiveCamera>
 
 <T.AmbientLight color="white" />
-<T.DirectionalLight color="beige" position={[0, 10, 20]} castShadow />
-<T.DirectionalLight color="white" position={[0, 20, 0]} castShadow />
+<T.DirectionalLight color="beige" position={[0, 10, 20]} />
+<T.DirectionalLight color="white" position={[0, 20, 0]} />
 
 {#if $model}
     <T.Mesh 
         bind:ref={faceMesh} 
         geometry={$model.nodes.mesh_0.geometry} 
-        receiveShadow castShadow 
         scale={$scale} 
-        scale.y={0.19} 
+        scale.y={0.37} 
         rotation.x={0.05} 
         rotation.y={$rotationX} 
         rotation.z={$rotationY} 
         position={[0, 6, 7]}
         on:click={({ point }) => {
-            pulsePosition.set(point.x, point.y, point.z)
-            pulseTimer.set(0, {
-              duration: 0
-            })
-            pulseTimer.set(1, {
-              duration: 5000
-            })
+            pulsePosition.set(point.x, point.y, point.z);
+            pulseTimer.set(0, { duration: 0 });
+            pulseTimer.set(1, { duration: 3000 });
           }}
     >
         <T.ShaderMaterial
             fragmentShader={fragment}
             vertexShader={vertex}
             uniforms={{
-            pulseTimer: {
-                value: 0
-            },
-            pulsePosition: {
-                value: pulsePosition
-            }
+                pulseTimer: { value: 0 },
+                pulsePosition: { value: pulsePosition }
             }}
             uniforms.pulseTimer.value={$pulseTimer}
         />
     </T.Mesh>
 {/if}
-
-<T.Mesh position.y={-1} receiveShadow>
-    <T.CylinderGeometry args={[6, 6, 0.25, 128, 128]} />
-    <T.MeshStandardMaterial color="white" />
-</T.Mesh>
-
