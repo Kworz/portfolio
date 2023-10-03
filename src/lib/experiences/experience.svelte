9c995c3f7a699dbe29f48604682c6b168b95d3f6
Kworz <romain.sapet@metalizz.com>
2023-10-03 17:30:21 +0200

    reduced space between experiences

--- src/lib/experiences/experience.svelte
+++ src/lib/experiences/experience.svelte
@@ -1,9 +1,15 @@
+<script lang="ts">
+
+    export let last = false;
+
+</script>
+
 <div class="flex gap-8">
     <div class="flex gap-4 flex-col items-center">
         <div class="h-4 mt-2 aspect-square bg-indigo-500 rounded-full" />
         <div class="w-[2px] mx-auto bg-indigo-300 grow"/>
     </div>
-    <div>
+    <div class:pb-8={!last}>
         <div class="flex gap-4 align-middle items-center">
             <slot name="lead" />
         </div>
