a3d3973a1064c00eec954e295ab374f03b3e66f0
Kworz <romain.sapet@metalizz.com>
2023-10-03 13:46:41 +0200

    moved navlink to a separate component

---
+++ src/lib/navlink.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+
+    export let href: string;
+    export let sectionHeight: number;
+    export let isLead: boolean;
+
+</script>
+
+
+<a 
+    {href}
+    style:--sec-height={sectionHeight + "%"}
+    style:--is-lead={isLead ? "0" : "1"}
+    class="text-white font-normal hover:text-zinc-300 duration-50 relative navlink"
+>
+    <slot />
+</a>
+
+<style>
+
+    .navlink::before {
+        content: "";
+        display: block;
+        background-color: white;
+        margin-left: calc(var(--is-lead) * calc(100% - var(--sec-height)));
+        height: 1px;
+        width: var(--sec-height);
+        position: absolute;
+        bottom: 0px;
+        left: 0;
+        transition: ease-in-out;
+        transition-duration: 50ms;
+    }
+
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,19 @@
 <script lang="ts">
     import "$lib/app.css";
+    import Navlink from "$lib/navlink.svelte";
     import { section } from "$lib/section";
     import { onMount } from "svelte";
 
     let sectionsHeight = {
         home: 100,
         experience: 0,
-        experience2: 0
+        projects: 0,
+        contact: 0
     };
 
     const handleScroll = () => {
 
-        const sections = Array.from(document.getElementsByTagName("section")) as Array<HTMLDivElement>;
+        const sections = Array.from(document.getElementsByTagName("section")) as Array<HTMLDivElement & { id: "home" | "experience" | "projects" | "contact"}>;
 
         for(const s of sections)
         {
@@ -36,30 +38,14 @@
 
 <svelte:window on:scroll={handleScroll} />
 
-<nav class="fixed right-8 top-8 flex flex-row gap-6 items-center text-white text rounded-full backdrop-blur-lg py-2 px-4 bg-zinc-900/50">
-    <a href="#home" style="--sec-height: {sectionsHeight["home"]}%; --is-lead: {$section === "home" ? "0" : "1"}" class="hover:text-zinc-300 duration-100 relative navlink">Présentation</a>
+<nav class="fixed right-8 top-8 flex flex-row gap-4 items-center text-white text rounded-full backdrop-blur-lg py-2 px-5 bg-zinc-900/50">
+    <Navlink href="#home" sectionHeight={sectionsHeight["home"]} isLead={$section === "home"}>Présentation</Navlink>
     <div class="rounded-full h-1 w-1 bg-white"></div>
-    <a href="#experience" style="--sec-height: {sectionsHeight["experience"]}%; --is-lead: {$section === "experience" ? "0" : "1"}" class="hover:text-zinc-300 duration-100 relative navlink">Experience</a>
+    <Navlink href="#experience" sectionHeight={sectionsHeight["experience"]} isLead={$section === "experience"}>Experience</Navlink>
     <div class="rounded-full h-1 w-1 bg-white"></div>
-    <a href="#experience2" style="--sec-height: {sectionsHeight["experience2"]}%; --is-lead: {$section === "experience2" ? "0" : "1"}" class="hover:text-zinc-300 duration-100 relative navlink">Experience2</a>
+    <Navlink href="#projects" sectionHeight={sectionsHeight["projects"]} isLead={$section === "projects"}>Projets</Navlink>
+    <div class="rounded-full h-1 w-1 bg-white"></div>
+    <Navlink href="#contact" sectionHeight={sectionsHeight["contact"]} isLead={$section === "contact"}>Contact</Navlink>
 </nav>
 
 <slot />
-
-<style>
-
-    .navlink::before {
-        content: "";
-        display: block;
-        background-color: white;
-        margin-left: calc(var(--is-lead) * calc(100% - var(--sec-height)));
-        height: 1px;
-        width: var(--sec-height);
-        position: absolute;
-        bottom: 0px;
-        left: 0;
-        transition: ease-in-out;
-        transition-duration: 50ms;
-    }
-
-</style>
