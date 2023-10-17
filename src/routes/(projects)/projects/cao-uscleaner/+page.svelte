b3e7793f4f0160de250e522ee71e0f6bb7cda5b2
Kworz <romain.sapet@metalizz.com>
2023-10-17 22:03:58 +0200

    created uscleaner cao project

---
+++ src/routes/(projects)/projects/cao-uscleaner/+page.svelte
@@ -0,0 +1,70 @@
+<h1>USCleaner</h1>
+
+
+<p>
+    L'USCleaner est une machine de nettoyage de pièces plastiques ou métal qui utilise les Ultrasons comme vecteur principal de nettoyage.
+    Durant mes premieres années chez Metalizz, j'ai travailler sur la conception et la fabrication de cette machine.
+</p>
+
+<img src="/projects/cao-uscleaner/uscleaner.webp" alt="USCleaner" class="h-96 my-8 mx-auto rounded-lg ring-2 ring-white bg-white px-4" />
+
+<h2 class="mt-6 mb-4">Conception</h2>
+
+<h3 class="mt-4 mb-2">Environement technique</h3>
+
+<p>
+    Toute la conception 3D, a été réalisée avec <a href="https://www.solidworks.com/fr">Solidworks</a>.
+    J'ai déssiné les schémas électriques et pneumatiques avec <a href="https://qelectrotech.org/">QElectrotech</a> logiciel open-source.
+</p>
+
+<h3 class="mt-4 mb-2">Chassis</h3>
+
+<p>
+    La base de cette machine est un chassis en profilé aluminium de <em>40 * 40 mm</em>. Ce chassis est ensuite équipé de panneau en dibond de 3 mm pour former la structure de la machine.
+</p>
+
+<p>
+    Nous utilisons un bac Ultrasons du commerce que nous avons intégré dans la machine.
+    Ce bac est ensuite équipé d'un système de régulation de température et de filtration a l'aide d'un échangeur thermique et d'une pompe de filtration.
+</p>
+
+<h3 class="mt-4 mb-2">Impression 3D</h3>
+
+<p>
+    Pour ce projet nous avons beaucoup eu recours a l'impression 3D, tout particulièrement la technologie <a href="https://www.eos.info/en/additive-manufacturing/selective-laser-sintering">SLS (Selective Laser Sintering)</a>.
+    Cette technologie permet de fabriquer des pièces en Polyamide (PA12 / Nylon), rapidement et avec une très bonne résistance mécanique et chimique. 
+</p>
+
+<p>
+    L'avantage de l'impression 3D SLS, C'est qu'elle permet une grande liberté de conception. 
+    Elle permet de fabriquer des pièces complexes sans avoir a les assembler comme on le ferait avec les techniques de fabrication traditionelles telles que L'injection ou le Rotomoulage.
+</p>
+
+<p>
+    Nous avons donc utilisé cette technologie pour fabriquer un vingtaine de pièce de l'USCleaner.
+</p>
+
+<h3 class="mt-4 mb-2">Paniers</h3>
+
+<p>
+    La particularité de cette machine, c'est que les pièces peuvent etre disposées dans un tambour rotatif. 
+    Ce tambour est entrainé par magnétisme et permet de faire tourner les pièces dans le liquide présent dans le bac.
+    Ce système permet de nettoyer les pièces de manière homogène et de réduire le temps de nettoyage.
+</p>
+
+<img src="/projects/cao-uscleaner/panier.webp" alt="Panier pour pièces" class="h-48 my-8 mx-auto rounded-lg ring-2 ring-white bg-white px-4" />
+
+<h3 class="mt-4 mb-2">Logiciel</h3>
+
+<p>
+    Cette machine embarque le logiciel <a href="/projects/nuster-kit">Nuster</a> que j'ai développé pour Metalizz.
+    Nuster est configuré pour supporter la regulation de température, et les cycles de nettoyage.
+</p>
+
+<h2 class="mt-6 mb-4">Note personelle</h2>
+
+<p class="font-semibold">
+    L'USCleaner est le projet qui m'as permis de diversifier Nuster, j'ai pu y ajouter la regulation de température par exemple.
+    J'ai aussi pu rédiger tout le dossier d'industrialisation de ce produit, en allant de la nomenclature complète (plus de 200 références) jusqu'au plan de montage.
+</p>
+
