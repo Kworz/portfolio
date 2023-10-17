ca68e0de3e501f8eb0ae2f538bb60531bd1f6de8
Kworz <romain.sapet@metalizz.com>
2023-10-17 22:04:20 +0200

    enhanced responsive for projects

--- src/routes/(projects)/projects/+layout.svelte
+++ src/routes/(projects)/projects/+layout.svelte
@@ -1,4 +1,4 @@
-<article class="w-1/2 mx-auto my-24 text-lg text-white">
+<article class="w-4/5 lg:w-2/3 xl:w-1/2 mx-auto my-8 md:my-12 lg:my-16 xl:my-24 text-lg text-white">
     <a href="/#projects" class="mb-4 inline-block text-base">Retour a la liste de mes projets</a>
     <slot />
 </article>
