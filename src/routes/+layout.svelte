e0fcc541d5d43a4d9ae00a3419bedfcb9efc181f
Kworz <romain.sapet@metalizz.com>
2024-01-28 21:42:28 +0100

    added last publish date

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,13 @@
 <script>
     import "$lib/app.css";
+    import { PUBLIC_PUBLISH_DATE } from "$env/static/public";
 </script>
 
-
 <slot />
+
+<footer class="bg-zinc-700 py-2 text-center">
+    <p class="text-white">
+        Dernière mise à jour: 
+        <span class="font-semibold">{PUBLIC_PUBLISH_DATE}</span>
+    </p>
+</footer>
