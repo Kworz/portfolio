f02a971965517d75005e028fd37f7dfd9925f067
Kworz <romain.sapet@metalizz.com>
2024-01-28 21:15:09 +0100

    added small sizes responsive menu

--- src/routes/(home)/+layout.svelte
+++ src/routes/(home)/+layout.svelte
@@ -2,11 +2,36 @@
     import "$lib/app.css";
     import Navlink from "$lib/navlink.svelte";
     import LoadIndicator from "$lib/LoadIndicator.svelte";
+    import Bars from "$lib/bars.svelte";
+    import { fade } from "svelte/transition";
+    import { page } from "$app/stores";
+
+    let expandSmallMenu = false;
+
+    $: $page.url, expandSmallMenu = false;
 </script>
 
 <LoadIndicator />
 
-<nav class="z-10 hidden lg:flex fixed right-8 top-8 flex-row gap-4 items-center text-white text rounded-full backdrop-blur-lg duration-100 py-2 px-5 bg-zinc-900/50">
+<nav class="z-10 flex md:hidden fixed top-0 right-0 left-0 py-2 px-5 bg-white border-b-[1px] border-zinc-300/75 flex-col gap-2">
+
+    <button class="flex gap-4 items-center" on:click={() => expandSmallMenu = !expandSmallMenu}>
+        <Bars />
+        <span class="font-semibold uppercase">Menu</span>
+    </button>
+
+    {#if expandSmallMenu}
+        <div class="flex flex-col gap-2 duration-100" in:fade out:fade>   
+            <a href="#home">Présentation</a>
+            <a href="#projects">Projets</a>
+            <a href="#experience">Experience</a>
+            <a href="#contact">Contact</a>
+        </div>
+    {/if}
+
+</nav>
+
+<nav class="z-10 hidden md:flex fixed right-8 top-8 flex-row gap-4 items-center text-white text rounded-full backdrop-blur-lg duration-100 py-2 px-5 bg-zinc-900/50">
     <div class="scrollmask" />
     <Navlink href="#home">Présentation</Navlink>
     <div class="rounded-full h-1 w-1 bg-white"></div>
