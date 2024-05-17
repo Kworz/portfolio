40b56566e89e31485912312d407791da64b96152
Kworz <romain.sapet@metalizz.com>
2024-05-17 09:11:25 +0200

    updated eidos-coating project page

--- src/routes/(projects)/projects/eidos-coating/+page.svelte
+++ src/routes/(projects)/projects/eidos-coating/+page.svelte
@@ -2,22 +2,15 @@
 
 <h2>Description du projet</h2>
 
-<p class="text-orange-500 font-semibold mt-4">
-    ⚠️ Attention: Le site n'est pas encore terminé, il est en cours de developpement.
-</p>
-
 <p class="mt-8">
-    J'ai réalisé pour l'entreprise <a href="https://eidoscoating.com">Eidos coating</a> leur site vitrine pour présenter leur activité.
-</p>
-
-<p>
+    J'ai réalisé pour l'entreprise <a href="https://eidoscoating.com">Eidos coating</a> leur site vitrine pour présenter leur activité. <br>
     Comme vu avec le client, j'ai réalisé un site simple et efficace sans CMS pour éviter les problèmes de sécurité et de maintenance.
 </p>
 
 <p>
-    Ce site est construit avec <a href="https://svelte.dev">Svelte</a> et <a href="https://svelte.dev">SvelteKit</a>.
+    Ce site est construit avec <a href="https://svelte.dev">Svelte</a> et <a href="https://svelte.dev">SvelteKit</a>.<br>
     J'ai utilisé <a href="https://tailwindcss.com">TailwindCSS</a> pour le style des composants et des pages.
 </p>
 
-<img src="/projects/eidos-coating/homepage.png" alt="Page d'accueil site eidoscoating.com" class="rounded-xl my-8" />
+<img src="/projects/eidos-coating/homepage.png" alt="Page d'accueil site eidoscoating.com" class="rounded-xl my-8 ring-1 ring-white" />
 
